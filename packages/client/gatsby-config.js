476102050725ca6f8371a321cb9fcdcf31ac6800
Donghyun Seo <donghyun.seo91@gmail.com>
2020-03-07 21:20:04 +0900

    fix: Fix build error

--- packages/client/gatsby-config.js
+++ packages/client/gatsby-config.js
@@ -61,14 +61,6 @@ module.exports = {
         path: `${__dirname}/src/assets/images`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `data`,
-        path: `${__dirname}/src/assets/data/`,
-        ignore: [`**/\.*`],
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
 
