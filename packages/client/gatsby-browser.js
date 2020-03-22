0011c9fed2eebabc10d352f5c58a84946802dd67
Donghyun Seo <donghyun.seo91@gmail.com>
2020-03-22 20:16:57 +0900

    task: Fix for sw caching

--- packages/client/gatsby-browser.js
+++ packages/client/gatsby-browser.js
@@ -1,5 +1,10 @@
 import 'animate.css/animate.css';
 import './src/styles/common.css';
-
+import onServiceWorkerUpdateReady from "./src/libs/onServiceWorkerUpdateReady";
 import wrapWithProvider from './src/libs/wrapRootElement';
+
 export const wrapRootElement = wrapWithProvider;
+
+export {
+  onServiceWorkerUpdateReady
+};
---
+++ packages/client/src/libs/onServiceWorkerUpdateReady.ts
@@ -0,0 +1,3 @@
+export default function onServiceWorkerUpdateReady() {
+  window.location.reload(true);
+}
