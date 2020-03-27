919f8f1c44e8f51890b8331461f78f48aa667fae
Donghyun Seo <donghyun.seo91@gmail.com>
2020-03-27 23:32:19 +0900

    feat: Add search repository and use case
    issue: #6

---
+++ packages/core/src/domain/repositories/search/SearchRepository.ts
@@ -0,0 +1,12 @@
+import {
+  Observable,
+  of
+} from 'rxjs';
+import Repository from '../Repository';
+// --ADD_IMPORT--
+
+export default interface SearchRepository extends Repository {
+  searchQuery(prop: any): Observable<any>;
+
+  // --ADD_METHOD--
+}
