5a06bb2e825cd918d43c6d765b10fd5f908e18be
Donghyun Seo <donghyun.seo91@gmail.com>
2020-04-04 16:47:50 +0900

    env: Edit lerna env and edit functions

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,8 +1,13 @@
-import * as functions from 'firebase-functions';
+import { https } from 'firebase-functions';
+import axios from 'axios';
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+export const helloWorld = https.onRequest(async (request, response) => {
+  const searches = await axios('https://www.google.co.kr/complete/search?q=g&cp=1&client=psy-ab&xssi=t')
+    .then(
+      res => res.data.replace(')]}\'', '')
+    );
+  response.send(searches);
 });
